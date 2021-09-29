3866d22f0fd0c5ae188984ba2fd32ab631263ffb
pedro.moreira@aoatechpartners.com.br <pedrohmoreira87@gmail.com>
2021-09-29 15:00:57 -0300

    understanding-observables

---
+++ angular/understanding-observables/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from './user.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit , OnDestroy{
+  userActivate = false;
+  private activatedSub: Subscription
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.activateEmitter.subscribe(didActivate => {
+      this.userActivate = didActivate;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
+}
